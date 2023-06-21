864d7647d95477df67245a18d3599728557772e1
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-06-22 08:00:19 +0900

    Update deps

--- denops/@ddu-sources/dummy.ts
+++ denops/@ddu-sources/dummy.ts
@@ -1,9 +1,9 @@
 import {
   BaseSource,
   Item,
-} from "https://deno.land/x/ddu_vim@v2.8.3/types.ts";
-import { Denops } from "https://deno.land/x/ddu_vim@v2.8.3/deps.ts";
-import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.4.0/file.ts";
+} from "https://deno.land/x/ddu_vim@v3.2.4/types.ts";
+import { Denops } from "https://deno.land/x/ddu_vim@v3.2.4/deps.ts";
+import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.5.0/file.ts";
 
 type Params = {
   word: string;
