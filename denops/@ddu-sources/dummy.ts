12c3ee35be09388ae0575a2706940fe42403429b
uga-rosa <uga6603@gmail.com>
2023-06-22 10:33:08 +0900

    deno fmt

--- denops/@ddu-sources/dummy.ts
+++ denops/@ddu-sources/dummy.ts
@@ -1,14 +1,11 @@
-import {
-  BaseSource,
-  Item,
-} from "https://deno.land/x/ddu_vim@v3.2.4/types.ts";
+import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.2.4/types.ts";
 import { Denops } from "https://deno.land/x/ddu_vim@v3.2.4/deps.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.5.0/file.ts";
 
 type Params = {
   word: string;
   display: string;
-}
+};
 
 export class Source extends BaseSource<Params> {
   override gather(args: {
